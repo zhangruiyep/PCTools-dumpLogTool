a0c91b667aa02e3f5d0c66f92a0cc5456947769b
zhangruiyep <zhangruiyep@outlook.com>
2017-03-01 19:52:03 +0800

    fix bug: exception in 32-bit OS

--- winRegistry.py
+++ winRegistry.py
@@ -5,6 +5,7 @@ def getRegValue(regPath, regName):
 
 	#print regPath, regName
 	value = ""
+	proc_arch64 = None
 	
 	try:
 		proc_arch = os.environ['PROCESSOR_ARCHITECTURE'].lower()
@@ -12,20 +13,28 @@ def getRegValue(regPath, regName):
 	except:
 		pass
 
-	if proc_arch == 'x86' and not proc_arch64:
-		arch_keys = {0}
+	#print proc_arch
+	#print proc_arch64
+	if proc_arch == 'x86' and proc_arch64 == None:
+		try:
+			key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, regPath, 0, _winreg.KEY_READ)
+			value = _winreg.QueryValueEx(key, regName)
+		except OSError:
+			print OSError.errno
+			pass
+
 	elif proc_arch == 'x86' or proc_arch == 'amd64':
 		arch_keys = {_winreg.KEY_WOW64_32KEY, _winreg.KEY_WOW64_64KEY}
+		for arch_key in arch_keys:
+			try:
+				key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, regPath, 0, _winreg.KEY_READ | arch_key)
+				value = _winreg.QueryValueEx(key, regName)
+				print arch_key, value
+			except OSError:
+				print OSError.errno
+				pass
 	else:
 		raise Exception("Unhandled arch: %s" % proc_arch)
 
-	for arch_key in arch_keys:
-		try:
-			key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, regPath, 0, _winreg.KEY_READ | arch_key)
-			value = _winreg.QueryValueEx(key, regName)
-			#print arch_key, value
-		except OSError:
-			#print OSError.errno
-			pass
 	
 	return value
